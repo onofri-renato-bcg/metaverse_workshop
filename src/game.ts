2d565eea3848dc7f500fc74f2d34af0eae1f6dc4
Onofri.Renato <onofri.renato@bcgplatinion.com>
2022-10-05 11:48:05 -0300

    first part of the workshop

---
+++ src/game.ts
@@ -0,0 +1,32 @@
+// import { Wall } from "./wall"
+
+// Base
+// const base = new Entity()
+// base.addComponent(new GLTFShape("models/baseDarkWithCollider.glb"))
+
+// engine.addEntity(base)
+
+// Facade
+// const facade = new Entity()
+// facade.addComponent(new GLTFShape("models/facade.glb"))
+// facade.addComponent(new Transform({ position: new Vector3(8, 0.05, 5) }))
+// facade.getComponent(Transform).rotate(Vector3.Up(), 180)
+
+// engine.addEntity(facade)
+
+// const wall_left = new Entity()
+// wall_left.addComponent(new BoxShape())
+// wall_left.addComponent(new Transform({ position: new Vector3(1, 0.05, 10), scale: new Vector3(10, 10, 0.5) }))
+// wall_left.getComponent(Transform).rotate(Vector3.Up(), 90)
+// engine.addEntity(wall_left)
+
+// const wall_left = new Wall(new Vector3(1, 0.05, 10))
+// wall_left.getComponent(Transform).rotate(Vector3.Up(), 90)
+
+// const wall_right = new Wall(new Vector3(15, 0.05, 10))
+// wall_right.getComponent(Transform).rotate(Vector3.Up(), 90)
+
+// const wall_back = new Wall(new Vector3(8, 0.05, 15))
+// // const wall_back = new Wall(new Vector3(8, 0.05, 15), new Vector3(14, 10, 0.5))
+// wall_back.getComponent(Transform).rotate(Vector3.Up(), 180)
+
